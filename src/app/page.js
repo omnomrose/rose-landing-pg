6f730627d95e007584d6e886c1b928b15027559b
omnomrose <maryrosenguyenbusiness@gmail.com>
2024-03-19 22:33:12 -0700

    fixed cursor?

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
-'use client';
+'use client'
+import styles from './page.module.scss'
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import useMousePosition from './utils/useMousePosition';
-import styles from '@/app/page.module.css'
-import Header from '@/components/Header';
+
 export default function Home() {
+
   const [isHovered, setIsHovered] = useState(false);
   const { x, y } = useMousePosition();
-  const size = isHovered ? 300 : 40;
+  const size = isHovered ? 400 : 40;
 
   return (
     <main className={styles.main}>
-      <Header />
       <motion.div
         className={styles.mask}
         animate={{
@@ -20,16 +20,15 @@ export default function Home() {
         }}
         transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
       >
-        <p className={styles.para} onMouseEnter={() => { setIsHovered(true) }} onMouseLeave={() => { setIsHovered(false) }}>
-          I'm a <span className={styles.span}>UX/UI Designer</span> that knits solutions and intuitive experiences together.
+        <p onMouseEnter={() => { setIsHovered(true) }} onMouseLeave={() => { setIsHovered(false) }} className={styles.p}>
+          I&apos;m a UX/UI DESIGNER that knits solutions and intuitive experiences together.
         </p>
       </motion.div>
 
       <div className={styles.body}>
-        <p className={styles.para}>
-          I'm a <span>UX/UI Designer</span> that knits solutions and intuitive experiences together.
-        </p>
+        <p className={styles.p}>I&apos;m a <span className={styles.span}> UX/UI DESIGNER </span> that knits solutions and intuitive experiences together.</p>
       </div>
-    </main>
-  );
+
+    </main >
+  )
 }
